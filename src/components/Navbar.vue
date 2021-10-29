50c7e7def604a4107d9ebb4ea4f92a6e5c3964d6
wildantrf <wildantrf@gmail.com>
2021-10-29 13:49:30 +0700

    beres navbar page add list data

---
+++ src/components/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="navbarb">
+    <div class="navbarcontainer">
+      <div @click.prevent="TambahBarang" class="navbarbutton">
+        <a href="">Input Produk</a>
+      </div>
+      <div @click.prevent="Kasir" class="navbarbutton">
+      <a href="">Kasir</a>
+      </div>
+      <div class="navbarbutton">
+      <a href="">Pesanan</a>
+      </div>
+      <div class="navbarbutton">
+      <a href="">Selesai</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods : {
+    TambahBarang() {
+      this.$emit('TambaB','Asa')
+    },
+    Kasir() {
+      this.$emit('kasirS','Asa')
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+.navbarcontainer {
+  display:flex;
+  margin:auto;
+  text-align:center;
+
+}
+.navbarb {
+  display:flex;
+  background-color: darkcyan;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 80px;
+  text-align: center;
+  justify-content: center;
+  vertical-align: middle;
+  margin:auto;
+}
+
+a {
+ 
+  font-weight: bold;
+  color:white;
+  width: 100%;
+  height: 80px;
+  padding: 10px;
+  margin:auto;
+}
+</style>
