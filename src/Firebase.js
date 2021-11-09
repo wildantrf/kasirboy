62cd837c6d92cd31507669d182e48161899d2c2c
wildantrf <wildantrf@gmail.com>
2021-11-09 20:00:59 +0700

    gas

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,6 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
+  apiKey: "AIzaSyDl1iOYAi71_XoxsxCeEIMIGawRfT3wVok",
+  authDomain: "kasirboy-5800c.firebaseapp.com",
+  projectId: "kasirboy-5800c",
+  storageBucket: "kasirboy-5800c.appspot.com",
+  messagingSenderId: "337894839496",
+  appId: "1:337894839496:web:f8aa75f096505fcbd201fd",
+  measurementId: "G-G6M84MWY7R"
   
 };
  const app = initializeApp(firebaseConfig);
